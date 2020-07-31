ebb1c31cfab7505e5404b8c83ab528ef421c481c
ElsaV22 <elsa22@att.net>
2020-07-30 19:08:41 -0500

    Add project files.

---
+++ StaticExerciseUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExerciseUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature in Fahrenheit?");
+            var farenheit = int.Parse(Console.ReadLine());
+            var celcius = TempConverter.FahrenheitToCelsius(farenheit);
+            Console.WriteLine($"The temperature converted to Celcius is {celcius}");
+
+            Console.WriteLine("What is the temperature in Celcius?");
+            var celcius2 = int.Parse(Console.ReadLine());
+            var fahrenheit2 = TempConverter.CelsiusToFahrenheit(celcius2);
+            Console.WriteLine($"The temperature converted to Fahreneit is {fahrenheit2}");
+
+        }
+    }
+}
---
+++ StaticExerciseUI/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExerciseUI
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double farenheit)
+        {
+            var result = (farenheit - 32) /1.8;
+            return result;
+
+        }
+
+        public static double CelsiusToFahrenheit(double celcius)
+        {
+            return (celcius * 9) / 5 + 32;
+        
+        }
+    }
+
+
+}
